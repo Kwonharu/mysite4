686719441e0ffa8be52a05306fd763e4d2e37014
Kwonharu <byhank0@naver.com>
2023-09-15 17:41:39 +0900

    세션 넣기 전

---
+++ src/main/java/com/javaex/controller/UserController.java
@@ -0,0 +1,42 @@
+package com.javaex.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.javaex.dao.UserDao;
+import com.javaex.vo.UserVo;
+
+@Controller
+@RequestMapping(value="/user")
+public class UserController {
+	
+	@Autowired
+	private UserDao userDao;
+	
+	//로그인 폼
+	@RequestMapping(value="/loginForm", method = {RequestMethod.GET, RequestMethod.POST})
+	public String loginForm() {
+		System.out.println("UserController.loginForm()");
+	
+		return "user/loginForm";
+	}
+	
+	//로그인
+	@RequestMapping(value="/login", method = {RequestMethod.GET, RequestMethod.POST})
+	public String login(@ModelAttribute UserVo userVo) {
+		System.out.println("UserController.login");
+		
+		UserVo authUser = userDao.userSelectOne(userVo);
+		
+		
+		return "";
+	}
+	
+}
+
+
+
+
